e86381bddff68e41b012f043ee04d00514d223a6
nakajmg <nakajima.jmg@gmail.com>
2022-06-30 10:44:07 +0900

    update to v2

--- src/main.ts
+++ src/main.ts
@@ -4,35 +4,38 @@ import { toHtml } from "./modules/toHtml"
 import Display from "./components/Display.svelte"
 import Preview from "./components/Preview.svelte"
 ;((PLUGIN_ID) => {
-  kintone.events.on(["app.record.detail.show"], async (eventObject: KintoneEventObject) => {
-    const config = getPluginConfig(PLUGIN_ID)
-    const { record } = eventObject
-    await Promise.all(
-      config.targetFields.map(async (fieldCode) => {
-        const html = await toHtml(record[fieldCode].value as string, config.userData)
-        const el = getFieldElement(fieldCode)
-        const containerEl = el?.parentElement
-        const selector = ".control-value-gaia"
-        if (containerEl) {
-          containerEl.setAttribute("class", containerEl.getAttribute("class") + " yomiyasuin")
-          new Display({
-            target: containerEl,
-            props: {
-              html,
-              containerEl: containerEl,
-              valueSelector: selector,
-            },
-          })
-          containerEl.querySelector(selector)?.setAttribute("aria-hidden", "true")
-          containerEl.querySelector(selector)?.setAttribute("style", "display: none;")
-        }
-      })
-    )
-    return eventObject
-  })
+  kintone.events.on(
+    ["app.record.detail.show", "mobile.app.record.detail.show"],
+    async (eventObject: KintoneEventObject) => {
+      const config = getPluginConfig(PLUGIN_ID)
+      const { record } = eventObject
+      await Promise.all(
+        config.targetFields.map(async (fieldCode) => {
+          const html = await toHtml(record[fieldCode].value as string, config.userData)
+          const el = getFieldElement(fieldCode)
+          const containerEl = el?.parentElement
+          const selector = ".control-value-gaia"
+          if (containerEl) {
+            containerEl.setAttribute("class", containerEl.getAttribute("class") + " yomiyasuin")
+            new Display({
+              target: containerEl,
+              props: {
+                html,
+                containerEl: containerEl,
+                valueSelector: selector,
+              },
+            })
+            containerEl.querySelector(selector)?.setAttribute("aria-hidden", "true")
+            containerEl.querySelector(selector)?.setAttribute("style", "display: none;")
+          }
+        })
+      )
+      return eventObject
+    }
+  )
 
   kintone.events.on(
-    ["app.record.create.show", "app.record.edit.show"],
+    ["app.record.create.show", "app.record.edit.show", "app."],
     async (eventObject: KintoneEventObject) => {
       const config = getPluginConfig(PLUGIN_ID)
       config.targetFields.map((fieldCode) => {
